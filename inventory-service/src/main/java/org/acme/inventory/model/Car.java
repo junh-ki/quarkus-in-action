19ed949eaf1de94ade8444d7ee635384ae27688e
Junhyung Ki <junhyung.ki@hdi.de>
2025-07-30 23:00:32 +0200

    Add the default no args constructor for Car to avoid error when starting the application

--- inventory-service/src/main/java/org/acme/inventory/model/Car.java
+++ inventory-service/src/main/java/org/acme/inventory/model/Car.java
@@ -2,8 +2,10 @@ package org.acme.inventory.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Car {
 
