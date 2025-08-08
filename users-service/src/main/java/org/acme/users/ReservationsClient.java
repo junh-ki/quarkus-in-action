161d37a0b842053220c33fa828b6b9da75ef5534
Junhyung Ki <junhyung.ki@hdi.de>
2025-08-08 20:41:21 +0200

    Add ReservationsClient that makes REST calls to reservation-service in users-service

---
+++ users-service/src/main/java/org/acme/users/ReservationsClient.java
@@ -0,0 +1,30 @@
+package org.acme.users;
+
+import io.quarkus.oidc.token.propagation.common.AccessToken;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import org.acme.users.model.Car;
+import org.acme.users.model.Reservation;
+import org.jboss.resteasy.reactive.RestQuery;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+@AccessToken
+@Path("reservation")
+public interface ReservationsClient {
+
+    @GET
+    @Path("all")
+    Collection<Reservation> allReservations();
+
+    @POST
+    Reservation make(final Reservation reservation);
+
+    @GET
+    @Path("availability")
+    Collection<Car> availability(
+        @RestQuery final LocalDate startDate,
+        @RestQuery final LocalDate endDate);
+}
---
+++ users-service/src/main/java/org/acme/users/model/Reservation.java
@@ -0,0 +1,19 @@
+package org.acme.users.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+
+    private Long id;
+    private Long carId;
+    private String userId;
+    private LocalDate startDay;
+    private LocalDate endDay;
+}
