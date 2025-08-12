d376b3f9801111abdb0749394d8e0a4567e6a450
Junhyung Ki <junhyung.ki@hdi.de>
2025-08-12 11:48:20 +0200

    Add an example set of automatically generated CRUD endpoints by extending the PanacheEntityResource interface from the rest data lib

---
+++ reservation-service/src/main/java/org/acme/reservation/rest/ReservationCrudResource.java
@@ -0,0 +1,11 @@
+package org.acme.reservation.rest;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+import io.quarkus.rest.data.panache.ResourceProperties;
+import org.acme.reservation.entity.Reservation;
+
+/**
+ * This will automatically expose the CRUD endpoints without controller
+ */
+@ResourceProperties(path = "/admin/reservation")
+public interface ReservationCrudResource extends PanacheEntityResource<Reservation, Long> {}
